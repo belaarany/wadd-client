0e257de7452ba4314fd605b25d1596e430c9b9b8
Bela Arany <git@belaarany.com>
2021-11-28 14:16:01 +0100

    firebase removed

--- src/services/data-access/_deprecated/_strategies/index.ts
+++
@@ -1,5 +0,0 @@
-import { firestoreDatabaseStrategy } from "./firebase-firestore-database"
-import * as realtimeDatabaseStrategy from "./firebase-realtime-database"
-
-export const databaseStrategy = realtimeDatabaseStrategy
-// export const databaseStrategy = firestoreDatabaseStrategy
